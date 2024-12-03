43643267e1e66ac9e21405f24c034e6b1372b643
Lisanna <145480358+olesiakazakova@users.noreply.github.com>
2024-12-03 14:15:05 +0500

    Update Transform.java

--- src/Transform.java
+++ src/Transform.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public interface Transform {
+public class Transform {
     //задание 3.1 (метод)
     static <T, P> List<P> transformList(List<T> list, Function<T, P> function) {
         List<P> result = new ArrayList<>();
