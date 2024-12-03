416c6c622e75f837b3fc04b870a599a88d86b2cb
Lisanna <145480358+olesiakazakova@users.noreply.github.com>
2024-12-03 14:14:26 +0500

    Update Reducer.java

--- src/Reducer.java
+++ src/Reducer.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public interface Reducer {
+public class Reducer {
     //задание 3.3 (метод)
     static <T> T reduceList(List<T> list, Function<List<T>, T> reducer) {
         if (list == null || list.isEmpty()) return null;
